f98ed5d552cc052ea891e017c7df0ed204d5ffde
belvanaufal <qhikizaalicia@gmail.com>
2024-03-17 20:56:17 +0700

    TP Modul5

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPmodul5_1302220011
+{
+    internal class Class1
+    {
+    }
+}
